d6b94d6656af44fc1df09c5a5d9ba7ad4d4907db
sgledhill1695 <samgledhill1695@hotmail.com>
2024-02-13 17:14:59 +0000

    cv updated

--- app/lib/experience.jsx
+++ app/lib/experience.jsx
@@ -81,7 +81,7 @@ export default function Experience({navigation, setNavigation}){
     
             </section>
     
-            <a href="/downloads/Samuel-Gledhill-CV.pdf" target="_blank" className="flex items-center gap-2 sans-bold tracking-widest text-[13px]  text-[white] hover:text-[#5EEAD4] fill-[white] hover:fill-[#5EEAD4] mt-[35px]">
+            <a href="/downloads/samuel-gledhill-c-v.pdf" target="_blank" className="flex items-center gap-2 sans-bold tracking-widest text-[13px]  text-[white] hover:text-[#5EEAD4] fill-[white] hover:fill-[#5EEAD4] mt-[35px]">
                 <div className="hover:cursor-pointer uppercase">View Full Curriculum Vitae</div>
                 <svg xmlns="http://www.w3.org/2000/svg" className="w-[12px] hover:cursor-pointer" viewBox="0 0 384 512"><path d="M64 0C28.7 0 0 28.7 0 64V448c0 35.3 28.7 64 64 64H320c35.3 0 64-28.7 64-64V160H256c-17.7 0-32-14.3-32-32V0H64zM256 0V128H384L256 0zM216 232V334.1l31-31c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-72 72c-9.4 9.4-24.6 9.4-33.9 0l-72-72c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0l31 31V232c0-13.3 10.7-24 24-24s24 10.7 24 24z" /></svg>
             </a>
